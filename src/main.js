4d90a3d8ce8f21cc491fe0d73fda3a58dea27de5
ahr-ahr <mp2pq3b8@outlook.com>
2026-08-18 19:19:10 +0700

    feat: add scene lighting

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+
+directionalLight.position.set(5, 10, 5);
+scene.add(directionalLight);
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000,
 );
+
 camera.position.set(0, 0, 5);
 
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor(0x202020);
@@ -18,6 +29,7 @@ document.body.appendChild(renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
+
   renderer.render(scene, camera);
 }
 
