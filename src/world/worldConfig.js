30e22c9d34dda116669a6b152fc3645ea90e513a
ahr-ahr <mp2pq3b8@outlook.com>
2026-08-18 19:40:58 +0700

    feat: define world coordinate system

---
+++ src/world/worldConfig.js
@@ -0,0 +1,17 @@
+export const WORLD_CONFIG = {
+  units: {
+    meter: 1,
+  },
+
+  axes: {
+    x: "east-west",
+    y: "elevation",
+    z: "north-south",
+  },
+
+  origin: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
+};
